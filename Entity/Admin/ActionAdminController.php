eca297014532d008a3b1eb0006d62e0bd4309e53
brickstorm <bbergstorm@gmail.com>
2012-06-07 13:15:14 +0200

    user

--- Entity/Admin/ActionAdminController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Brickstorm\SolidRBundle\Controller\Admin;
-
-use Sonata\AdminBundle\Controller\CRUDController as Controller;
-
-class ActionAdminController extends Controller
-{
-
-}
--- Entity/Admin/ProjectAdminController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Brickstorm\SolidRBundle\Controller\Admin;
-
-use Sonata\AdminBundle\Controller\CRUDController as Controller;
-
-class ProjectAdminController extends Controller
-{
-
-}
