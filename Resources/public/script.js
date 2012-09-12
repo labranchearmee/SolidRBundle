2e5a47ea42ce2b229132e1ab4525ab34b402833e
kohr <bbergstorm@gmail.com>
2012-09-12 23:10:35 +0200

    update

--- Resources/public/script.js
+++ Resources/public/script.js
@@ -1,30 +1,32 @@
+$(document).ready(function() {
 
-// carousels
-$('.carousel').carousel({
-  interval: 2000
-})
-
-//modal
-var trigger = $('#trigger-payment a.btn');
-trigger.click(function(){
-
-  var quantity = $('select[name=quantity]').val();
-  var rythm    = $(this).data('rythm');
-  var url      = $('#trigger-payment').data('modal-url');
-
-  $('#modal .modal-body').load(url+'?recurrent='+rythm+'&quantity='+quantity, function(){
-    $('#modal').modal('show');
-    $('#modal form').submit(function(){
-      console.log($(this));
-      window.open($(this).attr('action')+'?'+$(this).serialize());
-      return false;
+  // carousels
+  $('.carousel').carousel({
+    interval: 2000
+  })
+  
+  //modal
+  var trigger = $('#trigger-payment a.btn');
+  trigger.click(function(){
+  
+    var quantity = $('select[name=quantity]').val();
+    var rythm    = $(this).data('rythm');
+    var url      = $('#trigger-payment').data('modal-url');
+  
+    $('#modal .modal-body').load(url+'?recurrent='+rythm+'&quantity='+quantity, function(){
+      $('#modal').modal('show');
+      $('#modal form').submit(function(){
+        console.log($(this));
+        window.open($(this).attr('action')+'?'+$(this).serialize());
+        return false;
+      });
     });
   });
+  
+  $('#modal .btn-primary').live('click', function(){
+    $('#modal form').submit();
+  });
+  
+  //popovers
+  $('.trigger-popover').popover({trigger: 'hover'});
 });
-
-$('#modal .btn-primary').live('click', function(){
-  $('#modal form').submit();
-});
-
-
-$('.trigger-popover').popover();
